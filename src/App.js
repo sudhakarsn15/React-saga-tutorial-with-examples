c2543c77533470e0def6eb0b2c96aefb7f03f9d8
sudhakarsn15 <35915646+sudhakarsn15@users.noreply.github.com>
2021-03-09 16:03:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>React saga tutorial with examples</h1>
+      <h2>The initial setup</h2>
+      <code>
+        <pre>
+          $ cd redux-saga-beginner-tutorial
+          <br />$ npm install
+        </pre>
+      </code>
+      <p>To start the application, run:</p>
+      <code>$ npm start</code>
+      <p>Create a file sagas.js then add the following snippet:</p>
+      <pre>
+        <code>
+          export function* helloSaga()
+          <br />
+          {console.log("Hello Sagas!")}
+        </code>
+      </pre>
     </div>
   );
 }
